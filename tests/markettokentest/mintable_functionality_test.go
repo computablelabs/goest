2ca5b14fffc0dcad83fa56649e3c28274ccedab3
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2019-05-31 18:23:22 -0700

    Deploying custom contract for minting

--- tests/markettokentest/mintable_functionality_test.go
+++ tests/markettokentest/mintable_functionality_test.go
@@ -1,6 +1,7 @@
 package markettokentest
 
 import (
+	"github.com/computablelabs/goest/contracts/markettoken"
 	"github.com/computablelabs/goest/tests/test"
 	"github.com/ethereum/go-ethereum/common"
 	"math/big"
@@ -33,18 +34,29 @@ func TestBalanceOf(t *testing.T) {
 }
 
 func TestMint(t *testing.T) {
+	// Market Token: { consumes: [none], privileged: [listing, reserve] }
+	mtBal := big.NewInt(test.ONE_WEI * 5)
+	_, _, marketTokenCont, marketTokenErr := markettoken.DeployMarketToken(
+		context.AuthOwner,
+		context.Blockchain,
+		context.AuthOwner.From,
+		mtBal,
+	)
+	test.IfNotNil(t, marketTokenErr, "Error deploying markettoken")
+	context.Blockchain.Commit()
+
 	// only a privileged contract may call for mint, have a user pose as Listing
-	_, privErr := deployed.MarketTokenContract.SetPrivileged(test.GetTxOpts(context.AuthOwner, nil, big.NewInt(test.ONE_GWEI*2),
+	_, privErr := marketTokenCont.SetPrivileged(test.GetTxOpts(context.AuthOwner, nil, big.NewInt(test.ONE_GWEI*2),
 		100000), context.AuthUser3.From, deployed.ReserveAddress)
 	test.IfNotNil(t, privErr, "Error setting Market Token privileged contracts")
 
 	context.Blockchain.Commit()
 
-	supply, _ := deployed.MarketTokenContract.TotalSupply(nil)
-	ownerBal, _ := deployed.MarketTokenContract.BalanceOf(nil, context.AuthOwner.From)
-	listBal, _ := deployed.MarketTokenContract.BalanceOf(nil, context.AuthUser3.From) // our imposter
+	supply, _ := marketTokenCont.TotalSupply(nil)
+	ownerBal, _ := marketTokenCont.BalanceOf(nil, context.AuthOwner.From)
+	listBal, _ := marketTokenCont.BalanceOf(nil, context.AuthUser3.From) // our imposter
 	// minting will add to the balances...
-	_, mintErr := deployed.MarketTokenContract.Mint(test.GetTxOpts(context.AuthUser3, nil, big.NewInt(test.ONE_GWEI*2),
+	_, mintErr := marketTokenCont.Mint(test.GetTxOpts(context.AuthUser3, nil, big.NewInt(test.ONE_GWEI*2),
 		100000), big.NewInt(test.ONE_WEI*2))
 	test.IfNotNil(t, mintErr, "Error minting tokens")
 
@@ -52,17 +64,17 @@ func TestMint(t *testing.T) {
 
 	// our imposter
 	expectedListBal := listBal.Add(listBal, big.NewInt(test.ONE_WEI*2))
-	if listBalNow, _ := deployed.MarketTokenContract.BalanceOf(nil, context.AuthUser3.From); listBalNow.Cmp(expectedListBal) != 0 {
+	if listBalNow, _ := marketTokenCont.BalanceOf(nil, context.AuthUser3.From); listBalNow.Cmp(expectedListBal) != 0 {
 		t.Errorf("Expected %v to be %v", expectedListBal, listBalNow)
 	}
 
 	expectedSupply := supply.Add(supply, big.NewInt(test.ONE_WEI*2))
-	if newSupply, _ := deployed.MarketTokenContract.TotalSupply(nil); newSupply.Cmp(expectedSupply) != 0 {
+	if newSupply, _ := marketTokenCont.TotalSupply(nil); newSupply.Cmp(expectedSupply) != 0 {
 		t.Errorf("Expected %v to be %v", newSupply, expectedSupply)
 	}
 
 	// owner bal does not increase in a mint operation
-	if newOwnerBal, _ := deployed.MarketTokenContract.BalanceOf(nil, context.AuthOwner.From); newOwnerBal.Cmp(ownerBal) != 0 {
+	if newOwnerBal, _ := marketTokenCont.BalanceOf(nil, context.AuthOwner.From); newOwnerBal.Cmp(ownerBal) != 0 {
 		t.Errorf("Expected %v to be %v", newOwnerBal, ownerBal)
 	}
 }
