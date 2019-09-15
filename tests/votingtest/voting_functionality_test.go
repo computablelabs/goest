e34d64f8ad7d17f466fd91ce8a64a1196b6bc5b8
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2019-09-15 12:40:34 -0700

    increasing gas fixes one test

--- tests/votingtest/voting_functionality_test.go
+++ tests/votingtest/voting_functionality_test.go
@@ -11,7 +11,7 @@ import (
 func TestVote(t *testing.T) {
 	// auth member will need at least the stake
 	transErr := test.MaybeTransferMarketToken(context, deployed, context.AuthOwner, context.AuthUser1.From,
-		big.NewInt(test.ONE_GWEI))
+		big.NewInt(2*test.ONE_GWEI))
 	test.IfNotNil(t, transErr, "Error maybe transferring market tokens")
 
 	// member will need to have approved the voting contract to spend at least the stake
@@ -19,51 +19,57 @@ func TestVote(t *testing.T) {
 		big.NewInt(2*test.ONE_GWEI))
 	test.IfNotNil(t, incErr, "Error maybe transferring market token approval")
 
-	// we need a candidate, a privileged contract must add it...
-	_, candErr := deployed.ParameterizerContract.Reparameterize(test.GetTxOpts(context.AuthUser1, nil,
-		big.NewInt(test.ONE_GWEI*2), 200000), test.VOTE_BY, big.NewInt(100000))
-	test.IfNotNil(t, candErr, fmt.Sprintf("Error adding candidate: %v", candErr))
-	context.Blockchain.Commit()
-
 	// Get the hash
 	reparamHash, _ := deployed.ParameterizerContract.GetHash(nil, test.VOTE_BY, big.NewInt(100000))
 	t.Logf("reparamHash is %v", reparamHash)
 
-	// Check that there is a candidate now
+	// Check that there is not a candidate now
 	isCan, _ := deployed.VotingContract.IsCandidate(nil, reparamHash)
-	if !isCan {
+	if isCan {
+		t.Errorf("Expected isCandidate to be false before reparam submission, got: %v", isCan)
+	}
+
+	// we need a candidate, a privileged contract must add it...
+	_, candErr := deployed.ParameterizerContract.Reparameterize(test.GetTxOpts(context.AuthUser1, nil,
+		big.NewInt(test.ONE_GWEI*2), 1000000), test.VOTE_BY, big.NewInt(100000))
+	test.IfNotNil(t, candErr, fmt.Sprintf("Error adding candidate: %v", candErr))
+	context.Blockchain.Commit()
+
+	// Check that there is a candidate now
+	isCanNow, _ := deployed.VotingContract.IsCandidate(nil, reparamHash)
+	if !isCanNow {
 		t.Errorf("Expected isCandidate to be true, got: %v", isCan)
 	}
 
 	//bytes, _ := deployed.ParameterizerContract.GetHash(nil, big.NewInt(7), big.NewInt(99))
 
-	//// should be no votes atm
-	//_, _, _, _, preYea, _, _ := deployed.VotingContract.GetCandidate(nil, bytes)
+	// should be no votes atm
+	_, _, _, _, preYea, _, _ := deployed.VotingContract.GetCandidate(nil, reparamHash)
 
-	//if preYea.Cmp(big.NewInt(0)) != 0 {
-	//	t.Errorf("Expected number of votes to be 0, got: %v", preYea)
-	//}
+	if preYea.Cmp(big.NewInt(0)) != 0 {
+		t.Errorf("Expected number of votes to be 0, got: %v", preYea)
+	}
 
-	//// cast a yea vote
-	//_, voteErr := deployed.VotingContract.Vote(test.GetTxOpts(context.AuthUser1, nil,
-	//	big.NewInt(test.ONE_GWEI*2), 150000), bytes, big.NewInt(1))
-	//test.IfNotNil(t, voteErr, fmt.Sprintf("Error voting for candidate: %v", voteErr))
+	// cast a yea vote
+	_, voteErr := deployed.VotingContract.Vote(test.GetTxOpts(context.AuthUser1, nil,
+		big.NewInt(test.ONE_GWEI*2), 150000), reparamHash, big.NewInt(1))
+	test.IfNotNil(t, voteErr, fmt.Sprintf("Error voting for candidate: %v", voteErr))
 
-	//context.Blockchain.Commit()
+	context.Blockchain.Commit()
 
-	//// has been recorded in the candidate's votes
-	//_, _, _, _, yea, _, _ := deployed.VotingContract.GetCandidate(nil, bytes)
+	// has been recorded in the candidate's votes
+	_, _, _, _, yea, _, _ := deployed.VotingContract.GetCandidate(nil, reparamHash)
 
-	//if yea.Cmp(big.NewInt(0)) != 1 {
-	//	t.Errorf("Expected number of votes to be > 0, got: %v", yea)
-	//}
+	if yea.Cmp(big.NewInt(0)) != 1 {
+		t.Errorf("Expected number of votes to be > 0, got: %v", yea)
+	}
 
-	//// should see the stake bal at voting.stakes.address.hash
-	//stake, _ := deployed.VotingContract.GetStake(nil, bytes, context.AuthUser1.From)
+	// should see the stake bal at voting.stakes.address.hash
+	stake, _ := deployed.VotingContract.GetStake(nil, reparamHash, context.AuthUser1.From)
 
-	//if stake.Cmp(big.NewInt(0)) != 1 {
-	//	t.Errorf("Expected stake to be > 0, got: %v", stake)
-	//}
+	if stake.Cmp(big.NewInt(0)) != 1 {
+		t.Errorf("Expected stake to be > 0, got: %v", stake)
+	}
 }
 
 func TestPollClosed(t *testing.T) {
