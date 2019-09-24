7d455fc14afe69433c33e6a6d2d2e3ce569f6811
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2019-09-24 16:22:31 -0700

    separating into stages

--- recompile.sh
+++ recompile.sh
@@ -1,21 +1,26 @@
+# Run precompilation
 npm run precompile:token:ether:vyper
-npm run compile:token:ether:vyper
-npm run compile:token:ether:vyper:external
 npm run precompile:token:market:vyper
-npm run compile:token:market:vyper
-npm run compile:token:market:vyper:external
 npm run precompile:voting:vyper
-npm run compile:voting:vyper
-npm run compile:voting:vyper:external
 npm run precompile:parameterizer:vyper
-npm run compile:parameterizer:vyper
-npm run compile:parameterizer:vyper:external
 npm run precompile:reserve:vyper
-npm run compile:reserve:vyper
-npm run compile:reserve:vyper:external
 npm run precompile:datatrust:vyper
-npm run compile:datatrust:vyper
-npm run compile:datatrust:vyper:external
 npm run precompile:listing:vyper
-npm run compile:listing:vyper
+
+# Run externals compilation
+npm run compile:token:ether:vyper:external
+npm run compile:token:market:vyper:external
+npm run compile:voting:vyper:external
+npm run compile:parameterizer:vyper:external
+npm run compile:reserve:vyper:external
+npm run compile:datatrust:vyper:external
 npm run compile:listing:vyper:external
+
+# Kick off the actual compilation 
+npm run compile:token:ether:vyper
+npm run compile:token:market:vyper
+npm run compile:voting:vyper
+npm run compile:parameterizer:vyper
+npm run compile:reserve:vyper
+npm run compile:datatrust:vyper
+npm run compile:listing:vyper
